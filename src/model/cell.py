74a63004ac9e2830e15b99f3b64e5f960c9ec012
SoftLipsHardNips <nilstoltanso@gmail.com>
2018-02-07 18:21:20 +0100

    Added game parameters into separate file.

--- src/model/cell.py
+++ src/model/cell.py
@@ -1,7 +1,5 @@
 import numpy
-
-MOVESPEED = 5
-SPLITSPEED = 2  # Speed of just spawned cell
+from .parameters import *
 
 
 class Cell(object):
@@ -16,8 +14,8 @@ class Cell(object):
     def setMoveDirection(self, commandPoint):
         difference = numpy.subtract(commandPoint, [self.x, self.y])
         angle = numpy.arctan2(difference[1] , difference[0])
-        self.vx = MOVESPEED * numpy.cos(angle)
-        self.vy = MOVESPEED * numpy.sin(angle)
+        self.vx = CELL_MOVE_SPEED * numpy.cos(angle)
+        self.vy = CELL_MOVE_SPEED * numpy.sin(angle)
 
     def split(self):
         pass
--- src/model/field.py
+++ src/model/field.py
@@ -1,15 +1,13 @@
 from random import randint
 import numpy
 from .cell import Cell
+from .parameters import *
 
 # The Field class is the main field on which cells of all sizes will move
 # Its size depends on how many players are in the game
 # It always contains a certain number of viruses and collectibles and regulates their number and spawnings
 
-START_RADIUS = 30
-MAX_COLLECTIBLE_SPAWN_PER_UPDATE = 5
-COLLECTIBLE_SIZE = 4
-MAXCOLLECTIBECOUNT = 100
+
 
 
 class Field(object):
@@ -19,7 +17,7 @@ class Field(object):
         self.collectibles = []
         self.players = []
         self.viruses = []
-        self.maxCollectibleCount = MAXCOLLECTIBECOUNT
+        self.maxCollectibleCount = MAX_COLLECTIBLE_COUNT
 
     def initializePlayer(self, player):
         x = randint(0, self.width)
