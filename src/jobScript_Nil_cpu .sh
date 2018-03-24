0305ec68dfc6dfc8e741638f8a54d1c0d289c62b
NILOIDE <nilstoltanso@gmail.com>
2018-03-24 19:28:02 +0100

    added new jobScript and input file for Nil

---
+++ src/jobScript_Nil_cpu .sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --time=1-12:00:00
+#SBATCH --mem=16000
+#SBATCH --nodes=1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=antonwiehe@gmail.com
+module load Python/3.5.2-foss-2016a
+module load matplotlib/1.5.3-foss-2016a-Python-3.5.2
+module load tensorflow/1.3.1-foss-2016a-Python-3.5.2
+module load h5py/2.5.0-foss-2016a-Python-3.5.1-HDF5-1.8.16 
+python -O ./aigar.py < trainNoGui_Nil.txt
