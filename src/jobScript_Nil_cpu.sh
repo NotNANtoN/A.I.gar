cb56d1106e615e3523e2bf8cf1d92dcd2ab3a047
NILOIDE <nilstoltanso@gmail.com>
2018-03-27 19:34:28 +0200

    changes to jobScript

--- src/jobScript_Nil_cpu.sh
+++ src/jobScript_Nil_cpu.sh
@@ -3,7 +3,7 @@
 #SBATCH --mem=16000
 #SBATCH --nodes=1
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=antonwiehe@gmail.com
+#SBATCH --mail-user=n.stolt.anso@student.rug.nl
 module load Python/3.5.2-foss-2016a
 module load matplotlib/1.5.3-foss-2016a-Python-3.5.2
 module load tensorflow/1.3.1-foss-2016a-Python-3.5.2
