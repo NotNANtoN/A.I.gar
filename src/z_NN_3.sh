37c4f146ad1d1757b032a97d36cf96aa70953757
NILOIDE <nilstoltanso@gmail.com>
2018-05-08 18:33:40 +0200

    Included GCCcore module in jobscript

--- src/z_NN_3.sh
+++ src/z_NN_3.sh
@@ -7,5 +7,6 @@
 module load Python/3.5.2-foss-2016a
 module load matplotlib/1.5.3-foss-2016a-Python-3.5.2
 module load TensorFlow/1.6.0-foss-2018a-Python-3.6.4
-module load h5py/2.5.0-foss-2016a-Python-3.5.1-HDF5-1.8.16 
+module load h5py/2.5.0-foss-2016a-Python-3.5.1-HDF5-1.8.16
+module load GCCcore/4.9.3 
 srun python -O ./aigar.py < nil_input/NN_3.txt
