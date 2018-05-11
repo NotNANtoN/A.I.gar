98134d8e1b876018b6c6dadf3a5e9af23b6ac429
NILOIDE <nilstoltanso@gmail.com>
2018-05-11 02:10:08 +0200

    Anton taught me how to vim

--- src/jobScript_Nil.sh
+++ src/jobScript_Nil.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
-#SBATCH --time=2-23:00:00
-#SBATCH --partition=gpu
-#SBATCH --gres=gpu:2
+#SBATCH --time=1-08:00:00
 #SBATCH --mem=20000
 #SBATCH --nodes=1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=n.stolt.anso@student.rug.nl
-module load Python/3.5.2-foss-2016a
-module load matplotlib/1.5.3-foss-2016a-Python-3.5.2
-module load tensorflow/1.5.0-foss-2016a-Python-3.5.2-CUDA-9.1.85
-module load h5py/2.5.0-foss-2016a-Python-3.5.1-HDF5-1.8.16 
-python -O ./aigar.py < trainNoGui.txt
+#SBATCH --output=GSPF_11%j.out
+module load matplotlib/2.1.2-foss-2018a-Python-3.6.4
+module load TensorFlow/1.6.0-foss-2018a-Python-3.6.4
+module load h5py/2.7.1-foss-2018a-Python-3.6.4
+python -O ./aigar.py <<EOF
+0
+1000000
+0
+10000
+0
+0
+1
+GRID_SQUARES_PER_FOV
+9
+0
+0
+1
+EOF
