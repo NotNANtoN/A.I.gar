8f213957e19b8b0630d78759c333adc0759875a7
NILOIDE <nilstoltanso@gmail.com>
2018-05-27 23:54:22 +0200

    Changes to jobscript

--- src/jobScript_Nil.sh
+++ src/jobScript_Nil.sh
@@ -10,14 +10,13 @@ module load TensorFlow/1.6.0-foss-2018a-Python-3.6.4
 module load h5py/2.7.1-foss-2018a-Python-3.6.4
 python -O ./aigar.py <<EOF
 0
-1000000
 0
 10000
 0
 0
 1
-GRID_SQUARES_PER_FOV
-9
+CNN_REPRESENTATION
+True
 0
 0
 1
