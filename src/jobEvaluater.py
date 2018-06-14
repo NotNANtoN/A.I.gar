a96f058ceef0fa0297398eaf2357b22395c4118a
NotAnton <antonwiehe@gmail.com>
2018-06-14 17:57:18 +0200

    Job evaluation script

---
+++ src/jobEvaluater.py
@@ -0,0 +1,116 @@
+import os
+import numpy
+
+from scipy.stats import ttest_ind
+
+def getAllDirectories():
+    allDirs = []
+    path = "savedModels/"
+    fileList = os.listdir(path)
+
+    for directory in fileList:
+        if os.path.isdir(path + directory):
+            allDirs.append(directory)
+
+    return allDirs
+
+def findDefaults():
+    defaults = [directory for directory in  getAllDirectories() if "Default" in directory]
+
+    if len(defaults) == 0:
+        print("")
+        print("No default models found!")
+        quit()
+
+    print("Number of defaults: ", len(defaults))
+
+    return defaults
+
+
+def addMeanToDictionary(dictionary, name, path):
+    f = open(path)
+    massList = list(map(float, f))
+    meanVal = numpy.mean(massList)
+    try:
+        dictionary[name]
+    except KeyError:
+        dictionary[name] = []
+    dictionary[name].append(meanVal)
+    f.close()
+
+def getDistributions(directories):
+    distributions = []
+
+    for directory in directories:
+        directory_distributions = {"name":directory}
+        path = "savedModels/" + directory + "/"
+        subfolders = os.listdir(path)
+        for subfolder in subfolders:
+            if not os.path.isdir(path + subfolder):
+                continue
+            dataPath = path + subfolder + "/data/"
+            for dataFile in os.listdir(dataPath):
+                if "MassOverTime.txt" in dataFile:
+                    massIdx = dataFile.find("Mass")
+                    name = dataFile[:massIdx]
+                    addMeanToDictionary(directory_distributions, name, dataPath + dataFile)
+                if "Mean_Mass_" in dataFile:
+                    meanMassIdx = dataFile.find("Mean_Mass_")
+                    name = dataFile[meanMassIdx + 10:-4]
+                    addMeanToDictionary(directory_distributions, name, dataPath + dataFile)
+        distributions.append(directory_distributions)
+
+    return distributions
+
+
+def createEvaluationFiles(all_dists, default_dists):
+    basePath = "savedModels/"
+
+    for distribution_set in all_dists:
+        data = ""
+        currentName = distribution_set["name"]
+        path = basePath + currentName
+        print("path: ", path)
+
+        for key in distribution_set:
+            if key == "name":
+                continue
+            meanValList = distribution_set[key]
+            overallMeanVal = numpy.mean(meanValList)
+            overallStd = numpy.std(meanValList)
+            data += key + " mean value " + str(round(overallMeanVal, 1)) + " and std " + str(round(overallStd, 1)) +"\n"
+
+            for default_dist_set in default_dists:
+                default_name = default_dist_set["name"]
+                if currentName == default_name:
+                    continue
+
+                try:
+                    defaultMeanValList = default_dist_set[key]
+                    overallDefaultMeanVal = numpy.mean(defaultMeanValList)
+                    tVal, pVal = ttest_ind(meanValList, defaultMeanValList)
+                    comparison = " better " if overallMeanVal > overallDefaultMeanVal else " worse "
+                    data += "Has a " + str(round((1 - pVal) * 100, 3)) + "%\t probability to be" + comparison + "than " + default_name + \
+                            "'s mean of " + str(round(overallDefaultMeanVal, 1)) + "\n"
+                except KeyError:
+                    continue
+                except RuntimeWarning:
+                    pass
+
+            data += "\n"
+        evalFile = open(path + "/eval.txt", "w+")
+        evalFile.write(data)
+        evalFile.close()
+
+
+if __name__ == '__main__':
+    defaults = findDefaults()
+
+    default_distributions = getDistributions(defaults) # in the form of dictionaries
+
+    allDirectories = getAllDirectories()
+
+    all_distributions = getDistributions(allDirectories)
+
+    createEvaluationFiles(all_distributions, default_distributions)
+
